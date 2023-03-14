2469ecb7909741889fbd4e7a9aa6e4eb49846b98
username <celinefong4@gmail.com>
2023-03-13 20:46:07 -0600

    fixed animation

--- Assets/Script/FlyingEnemy1.cs
+++ Assets/Script/FlyingEnemy1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyingEnemyMoment : MonoBehaviour
+public class FlyingEnemy1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
