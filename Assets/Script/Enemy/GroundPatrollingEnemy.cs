fc08c3f121206eb1fa4a0da3e0b439ec2ea83fed
Richmond Naviza <naviza@ualberta.ca>
2023-04-02 17:22:07 -0600

    Refactor for Ground Patroller

---
+++ Assets/Script/Enemy/GroundPatrollingEnemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPatrollingEnemy : PlatformPatrollingEnemy
+{
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    { 
+        if (collision.GetComponent<DoorController>()|| collision.GetComponent<StationaryHazard>())
+        {
+            // ignore, it's just a door/stationary object
+            return;
+        }
+        // when a collider exits another collider, this function runs
+        nextStepIsOnTheGround = false;
+        Debug.Log("The collider exited");
+        if (activated){
+            Debug.Log(nextStepIsOnTheGround);
+        } else {
+            // in patrol mode
+            if (facingLeft){
+                Flip(Vector2.right); // face right
+            } else {
+                Flip(Vector2.left); // face left
+            }
+        }
+    }
+
+    protected override void OnTriggerExit2D(Collider2D collision){
+        if (collision.GetComponent<DoorController>()|| collision.GetComponent<StationaryHazard>())
+        {
+            // ignore, it's just a door/stationary object
+            return;
+        }
+        nextStepIsOnTheGround = true;
+        Debug.Log("The collider entered");
+    }
+}
