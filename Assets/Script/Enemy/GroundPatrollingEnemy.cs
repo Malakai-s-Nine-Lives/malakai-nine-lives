f91a11fd44d45b93d4348d5de7bb5776c977f614
Balreet Grewal <balreet@ualberta.ca>
2023-04-03 09:09:18 -0600

    Update Walking Cut Paused Bug

--- Assets/Script/Enemy/GroundPatrollingEnemy.cs
+++ Assets/Script/Enemy/GroundPatrollingEnemy.cs
@@ -6,13 +6,14 @@ public class GroundPatrollingEnemy : PlatformPatrollingEnemy
 {
     protected override void OnTriggerEnter2D(Collider2D collision)
     { 
-        if (collision.GetComponent<DoorController>()|| collision.GetComponent<StationaryHazard>())
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy")||collision.GetComponent<DoorController>()|| collision.GetComponent<StationaryHazard>())
         {
+            Debug.Log("Ignoring", collision.gameObject);
             // ignore, it's just a door/stationary object
             return;
         }
         // when a collider exits another collider, this function runs
-        nextStepIsOnTheGround = false;
+        nextStepIsOnTheGround = true;
         Debug.Log("The collider exited");
         if (activated){
             Debug.Log(nextStepIsOnTheGround);
