386482d9e26dc6959ea5bb8da0aba9eb93cf0df9
Balreet Grewal <balreet@ualberta.ca>
2023-04-04 19:41:24 -0600

    Fixed Audio bug and a level 4 bug

--- Assets/Script/UI/SettingsMenu.cs
+++ Assets/Script/UI/SettingsMenu.cs
@@ -6,20 +6,8 @@ using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-    [SerializeField] Slider volume_slider;
     public MainMenu script;
     public TMP_Text mode_text;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayerPrefs.SetFloat("musicVolume", 1);
-    }
-
-    public void changeVolume()  // Change the volume according to slider
-    {
-        AudioListener.volume = volume_slider.value;
-    }
 
     public void HardMode()  // If we select hard mode
     {
